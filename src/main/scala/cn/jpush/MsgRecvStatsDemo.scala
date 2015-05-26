bffb6489889313cc16de7690433d395b3230074c
wufeng <wufeng@jpush.cn>
2015-05-26 15:08:31 +0800

    offline stats first commit to git lab

---
+++ src/main/scala/cn/jpush/MsgRecvStatsDemo.scala
@@ -0,0 +1,20 @@
+package cn.jpush
+
+
+/**
+ * Created by fengwu on 15/5/25.
+ */
+class MsgRecvStatsDemo {
+
+
+
+
+    def main(args: Array[String]) {
+
+        val stattime = args(0)
+        val fields = stattime.split("\t")
+
+    }
+
+
+}
